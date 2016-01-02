6dc789db7808dbe3e67831d119f10881b11ac468
xxp-pc-yanjiao <xxpniu@qq.com>
2016-01-02 13:45:58 +0800

    修改商店入口 修改商店。
    Signed-off-by: xxp-pc-yanjiao <xxpniu@qq.com>

--- Unity/Assets/Scripts/UI/Windows/UIShop.cs
+++ Unity/Assets/Scripts/UI/Windows/UIShop.cs
@@ -1,3 +1,4 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,6 +12,46 @@ namespace Assets.Scripts.UI.Windows
 {
     partial class UIShop
     {
+
+        public enum ShowType
+        {
+            Gold, Coin
+        }
+
+        public class ItemGridCoinTableModel : TableItemModel<ItemGridCoinTableTemplate>
+        {
+            public ItemGridCoinTableModel() { }
+            public override void InitModel()
+            {
+                //todo
+            }
+
+            public ItemConfig ItemConfig { private set; get; }
+            private DimondStoreConfig _Config;
+            public DimondStoreConfig Config
+            {
+                set
+                {
+                    _Config = value;
+                    var item = ExcelToJSONConfigManager.Current.GetConfigByID<ItemConfig>(_Config.ID);
+                    ItemConfig = item;
+                    if (item == null) return;
+                    var Color = _Config.Sold_price <= DataManagers.GamePlayerManager.Singleton.Gold ?
+                        LanguageManager.Singleton["APP_GREEN"] : LanguageManager.Singleton["APP_RED"];
+                    Template.Bt_itemName.Text(item.Name);
+                    Template.lb_cost.text = string.Format(LanguageManager.Singleton["UIShop_Model_Cost"],
+                        string.Format(Color,
+                        _Config.Sold_price));
+
+                }
+                get
+                {
+                    return _Config;
+                }
+            }
+
+        }
+
         public class ItemGridTableModel : TableItemModel<ItemGridTableTemplate>
         {
             public ItemGridTableModel() { }
@@ -44,10 +85,10 @@ namespace Assets.Scripts.UI.Windows
                     var Color = _Config.Sold_price <= DataManagers.GamePlayerManager.Singleton.Gold ?
                         LanguageManager.Singleton["APP_GREEN"] : LanguageManager.Singleton["APP_RED"];
                     Template.Bt_itemName.Text(item.Name);
-                    Template.lb_cost.text = string.Format(LanguageManager.Singleton["UIShop_Model_Cost"], 
+                    Template.lb_cost.text = string.Format(LanguageManager.Singleton["UIShop_Model_Cost"],
                         string.Format(Color,
                         _Config.Sold_price));
-                   
+
                 }
                 get
                 {
@@ -63,6 +104,16 @@ namespace Assets.Scripts.UI.Windows
             bt_close.OnMouseClick((s, e) => {
                 HideWindow();
             });
+
+            t_coin.OnMouseClick((s, e) => {
+                Type = ShowType.Coin;
+                OnUpdateUIData();
+            });
+
+            t_gold.OnMouseClick((s, e) => {
+                Type = ShowType.Gold;
+                OnUpdateUIData();
+            });
         }
         public override void OnShow()
         {
@@ -71,36 +122,53 @@ namespace Assets.Scripts.UI.Windows
             OnUpdateUIData();
         }
 
+
+        private ShowType Type = ShowType.Gold;
         public override void OnUpdateUIData()
         {
-            base.OnUpdateUIData(); 
+            base.OnUpdateUIData();
+            switch (Type)
+            {
+                case ShowType.Coin:
+                    ShowCoin();
+                    break;
+                case ShowType.Gold:
+                    ShowGold();
+                    break;
+            }
+        }
+
+        private void ShowGold()
+        {
+            PackageView.ActiveSelfObject(true);
+            PackageViewCoin.ActiveSelfObject(false);
             var shopItems = ExcelToJSONConfigManager.Current.GetConfigs<ExcelConfig.StoreDataConfig>(
-                (item) =>
-                {
-                    #region Condition
-                    var unlockType = (StoreUnlockType)item.Unlock_type;
-                    switch (unlockType)
-                    {
-                        case StoreUnlockType.None:
-                            return true;
-                        case StoreUnlockType.BuildGetTargetLevel:
-                            var builds = Tools.UtilityTool.SplitKeyValues(item.Unlock_para1);
-                            foreach (var i in builds)
-                            {
-                                var build = DataManagers.BuildingManager.Singleton[i.Key];
-                                if (build == null) return false;
-                                if (build.Level < i.Value) return false;
-                            }
-                            return true;
-                        case StoreUnlockType.ExploreGetTarget:
-                            int explore = 0;
-                            if (!int.TryParse(item.Unlock_para1, out explore)) return false;
-                            if (DataManagers.GamePlayerManager.Singleton[DataManagers.PlayDataKeys.EXPLORE_VALUE] < explore) return false;
-                            return true;
-                    }
-                    return true;
-                    #endregion
-                });
+               (item) =>
+               {
+                   #region Condition
+                   var unlockType = (StoreUnlockType)item.Unlock_type;
+                   switch (unlockType)
+                   {
+                       case StoreUnlockType.None:
+                           return true;
+                       case StoreUnlockType.BuildGetTargetLevel:
+                           var builds = Tools.UtilityTool.SplitKeyValues(item.Unlock_para1);
+                           foreach (var i in builds)
+                           {
+                               var build = DataManagers.BuildingManager.Singleton[i.Key];
+                               if (build == null) return false;
+                               if (build.Level < i.Value) return false;
+                           }
+                           return true;
+                       case StoreUnlockType.ExploreGetTarget:
+                           int explore = 0;
+                           if (!int.TryParse(item.Unlock_para1, out explore)) return false;
+                           if (DataManagers.GamePlayerManager.Singleton[DataManagers.PlayDataKeys.EXPLORE_VALUE] < explore) return false;
+                           return true;
+                   }
+                   return true;
+                   #endregion
+               });
 
             ItemGridTableManager.Count = shopItems.Length;
             for (var i = 0; i < ItemGridTableManager.Count; i++)
@@ -108,6 +176,22 @@ namespace Assets.Scripts.UI.Windows
                 ItemGridTableManager[i].Model.Config = shopItems[i];
                 ItemGridTableManager[i].Model.OnInfoClick = OnInfoClick;
                 ItemGridTableManager[i].Model.OnClick = OnClick;
+            }
+        }
+
+        private void ShowCoin()
+        {
+            PackageView.ActiveSelfObject(false);
+            PackageViewCoin.ActiveSelfObject(true);
+            var shopData = ExcelConfig.ExcelToJSONConfigManager.Current.GetConfigs<ExcelConfig.DimondStoreConfig>();
+            ItemGridTableManager.Count = shopData.Length;
+            int index = 0;
+            foreach (var i in ItemGridCoinTableManager)
+            {
+                i.Model.Config = shopData[index];
+               // i.Model.OnItemClick = OnClickBuy;
+                //i.Model.SetDrag(shopData.Length >= 7);
+                index++;
             }
         }
 
--- Unity/Assets/Scripts/UI/Windows/UIShop.ui.cs
+++ Unity/Assets/Scripts/UI/Windows/UIShop.ui.cs
@@ -25,29 +25,56 @@ namespace Assets.Scripts.UI.Windows
 
             }
         }
+        public class ItemGridCoinTableTemplate : TableItemTemplate
+        {
+            public ItemGridCoinTableTemplate(){}
+            public UIButton Bt_itemName;
+            public UILabel lb_cost;
+            public UIButton bt_info;
+
+            public override void InitTemplate()
+            {
+                Bt_itemName = FindChild<UIButton>("Bt_itemName");
+                lb_cost = FindChild<UILabel>("lb_cost");
+                bt_info = FindChild<UIButton>("bt_info");
+
+            }
+        }
 
 
-        public UISprite character1;
         public UIButton bt_close;
-        public UILabel lb_title;
         public UIPanel PackageView;
         public UIGrid ItemGrid;
+        public UIToggle t_gold;
+        public UIToggle t_coin;
+        public UIPanel PackageViewCoin;
+        public UIGrid ItemGridCoin;
 
 
         public UITableManager<AutoGenTableItem<ItemGridTableTemplate, ItemGridTableModel>> ItemGridTableManager = new UITableManager<AutoGenTableItem<ItemGridTableTemplate, ItemGridTableModel>>();
+        public UITableManager<AutoGenTableItem<ItemGridCoinTableTemplate, ItemGridCoinTableModel>> ItemGridCoinTableManager = new UITableManager<AutoGenTableItem<ItemGridCoinTableTemplate, ItemGridCoinTableModel>>();
 
 
         public override void InitTemplate()
         {
             base.InitTemplate();
-            character1 = FindChild<UISprite>("character1");
             bt_close = FindChild<UIButton>("bt_close");
-            lb_title = FindChild<UILabel>("lb_title");
             PackageView = FindChild<UIPanel>("PackageView");
             ItemGrid = FindChild<UIGrid>("ItemGrid");
+            t_gold = FindChild<UIToggle>("t_gold");
+            t_coin = FindChild<UIToggle>("t_coin");
+            PackageViewCoin = FindChild<UIPanel>("PackageViewCoin");
+            ItemGridCoin = FindChild<UIGrid>("ItemGridCoin");
 
             ItemGridTableManager.InitFromGrid(ItemGrid);
+            ItemGridCoinTableManager.InitFromGrid(ItemGridCoin);
 
-        }       
+        }
+        public static UIShop Show()
+        {
+            var ui = UIManager.Singleton.CreateOrGetWindow<UIShop>();
+            ui.ShowWindow();
+            return ui;
+        }
     }
 }
