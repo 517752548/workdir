c2d38b5671b0e1e5c561d355d43370da432fb03d
xxp-macbook-pro <54249636@qq.com>
2016-05-24 23:19:43 +0800

    Signed-off-by: xxp-macbook-pro <54249636@qq.com>

--- Unity/Assets/Scripts/Editor/BuildTools.cs
+++ Unity/Assets/Scripts/Editor/BuildTools.cs
@@ -21,6 +21,40 @@ public class BuildTools
 
 	}
 
+	[MenuItem("GAME/INCREASE_VERSION")]
+	public static void IncreaseVerson()
+	{
+
+		if (EditorUtility.DisplayDialog ("Increase Version", 
+			string.Format("Decide to increase version? Current is {0}",PlayerSettings.bundleVersion),
+			"Yes", "Cancle"))
+		{
+
+			var buildVersion = PlayerSettings.bundleVersion;
+			var versions = buildVersion.Split ('.');
+
+			int major = int.Parse (versions [0]);
+			int build = int.Parse (versions [1]);
+			var develop = int.Parse (versions [2]);
+
+			develop += 1;
+			if (develop >= 10) {
+				build += 1;
+				develop = 0;
+				if (build >= 10) {
+					major += 1;
+					build = 0;
+				}
+			}
+
+			PlayerSettings.bundleVersion = string.Format ("{0}.{1}.{2}", major, build, develop);
+
+			EditorUtility.DisplayDialog ("Version Increased", 
+				string.Format ("Version From {0} To {1}", buildVersion, PlayerSettings.bundleVersion),
+				"Close");
+		}
+	}
+
 }
 
 
---
+++ Unity/Assets/SetRenderQuene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SetRenderQuene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<SpriteRenderer> ();
+		panelOfRoot= NGUITools.FindInParents<UIPanel> (this.gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (lastQuene != panelOfRoot.startingRenderQueue) {
+			lastQuene = panelOfRoot.sortingOrder;
+			render.sortingOrder = lastQuene +1;
+		}
+	}
+
+	private SpriteRenderer render;
+
+	private int lastQuene;
+
+	public UIPanel panelOfRoot;
+
+    
+}
